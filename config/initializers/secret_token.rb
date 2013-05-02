88c6390543f248ff16956b245cc429ce183f9ff1
Kevin Lin <deeph4ze@gmail.com>
2013-05-01 20:20:02 -0700

    chapter 4 just added css

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '2c4bc351d566ff3631a28e3482e3b63bc0ac5cd3c95a1f22df52a9a4f51e2964c515757c5f5f1c063cb97c041e3b889cdc85581ce0dbb4febc33a72846bdb4f1'
