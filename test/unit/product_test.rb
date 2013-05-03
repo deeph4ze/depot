408a157d94b9a6147cd87337fd3347cabc2f1964
Kevin Lin <deeph4ze@gmail.com>
2013-05-02 23:39:16 -0700

    fixed stupid text bug

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -51,8 +51,7 @@ class ProductTest < ActiveSupport::TestCase
   	end
 
   	bad.each do |name|
-      puts new_product(name).valid?
-  		assert new_product(name).valid?, "#{name} shouldn't be valid"
+  		assert new_product(name).invalid?, "#{name} shouldn't be valid"
   	end
 
   end
